d260aabf3875ec76757d3aaf775a4749baee4bfa
rikaalii <rikaali0602.gmail.com>
2023-09-22 20:30:24 +0700

    edit

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -15,10 +15,30 @@ class GameViewModel {
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
 
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
+        private fun getNextWord() {
+            currentWord = allWordsList.random()
+            val tempWord = currentWord.toCharArray()
+            tempWord.shuffle()
+
+            while (String(tempWord).equals(currentWord, false)) {
+                tempWord.shuffle()
+            }
+            if (wordsList.contains(currentWord)) {
+                getNextWord()
+            } else {
+                _currentScrambledWord = String(tempWord)
+                ++currentWordCount
+                wordsList.add(currentWord)
+            }
+        }
+
     }
 
     override fun onCleared() {
@@ -36,4 +56,5 @@ fun nextWord(): Boolean {
         true
     } else false
 }
+
 
